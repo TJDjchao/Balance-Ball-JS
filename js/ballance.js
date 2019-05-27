f8b07133a14743528ce24cc4d357412e99ab1ecd
Jiaojinchao <314279802@qq.com>
2019-05-27 21:39:12 +0800

    添加 skybox,添加地图：圆柱杆，立方体，障碍物

--- js/ballance.js
+++ js/ballance.js
@@ -14,7 +14,7 @@ Ammo().then(function(Ammo) {
     var camera, controls, scene, renderer;
     var terrainMesh, texture;
     var clock = new THREE.Clock();
-    var materialDynamic, materialStatic, materialInteractive;
+    var materialDynamic, materialStatic, materialInteractive,materialCylinder;
 
     // Physics variables
     var collisionConfiguration;
@@ -28,6 +28,7 @@ Ammo().then(function(Ammo) {
     var objectTimePeriod = 3;
     var timeNextSpawn = time + objectTimePeriod;
     var maxNumObjects = 30;
+    var lastObjectInMap=new Map();//地图中最后一块元素
 
     // 键盘动作
     var actions = {};
@@ -53,7 +54,7 @@ Ammo().then(function(Ammo) {
 
         camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.2, 2000 );
         camera.position.x = 0;
-        camera.position.y = 20;
+        camera.position.y = 10;
         camera.position.z = -20;
         camera.up.x = 0;
         camera.up.y = 1;
@@ -66,11 +67,15 @@ Ammo().then(function(Ammo) {
         renderer.setPixelRatio( window.devicePixelRatio );
         renderer.setSize( window.innerWidth, window.innerHeight );
 
-        var ambientLight = new THREE.AmbientLight( 0x404040 );
-        scene.add( ambientLight );
+        //背景
+        scene.background=new THREE.CubeTextureLoader()
+            .setPath('./textures/')
+            .load(['Sky_A_px.jpg','Sky_A_nx.jpg','Sky_A_py.jpg','Sky_A_ny.jpg','Sky_A_pz.jpg','Sky_A_nz.jpg']);
 
+
+        //光源
         var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
-        dirLight.position.set( 10, 10, 5 );
+        dirLight.position.set( -100, 100, 0 );
         scene.add( dirLight );
 
         //坐标轴可视化
@@ -80,10 +85,47 @@ Ammo().then(function(Ammo) {
         materialDynamic = new THREE.MeshPhongMaterial( { color:0xfca400 } );
         materialStatic = new THREE.MeshPhongMaterial( { color:0x999999 } );
         materialInteractive=new THREE.MeshPhongMaterial( { color:0x990000 } );
+        materialCylinder=new THREE.MeshPhongMaterial( { color:0xc0c0c0 } );
 
         container.innerHTML = "";
 
         container.appendChild( renderer.domElement );
+
+        /*//加载地图
+        var mtlLoader=new THREE.MTLLoader();
+        mtlLoader.setPath('./textures/');
+        mtlLoader.load('level1-6.mtl',function (material) {
+            var objLoader=new THREE.OBJLoader();
+            objLoader.setPath('./textures/');
+            objLoader.load('level1-6.obj',function (object) {
+                //将模型缩放并添加到场景当中
+                object.scale.set(0.4,0.4,0.4);
+                scene.add(object);
+
+                //加入 physicsWorld
+                var transform = new Ammo.btTransform();
+                transform.setIdentity();
+                transform.setOrigin(new Ammo.btVector3(0, 0, 0));
+                transform.setRotation(new Ammo.btQuaternion(0, 0, 0, 1));
+                var motionState = new Ammo.btDefaultMotionState(transform);
+
+                var localInertia = new Ammo.btVector3(0, 0, 0);
+                object.calculateLocalInertia(0, localInertia);
+
+                var rbInfo = new Ammo.btRigidBodyConstructionInfo(0, motionState, geometry, localInertia);
+                var body = new Ammo.btRigidBody(rbInfo);
+
+                body.setFriction(1);
+                //body.setRestitution(.9);
+                //body.setDamping(0.2, 0.2);
+
+                physicsWorld.addRigidBody( body );
+
+            }, function (error) {
+                console.log(error);
+            });
+        });*/
+
         //检测状态
         stats = new Stats();
         stats.domElement.style.position = 'absolute';
@@ -235,12 +277,11 @@ Ammo().then(function(Ammo) {
                 }
                 if(actions.acceleration){
                     let v=body.getLinearVelocity();
-                    console.log(v.z());
                     if(v.z()+acceleratedSpeed>maxSpeed){
                         body.setLinearVelocity(new Ammo.btVector3(v.x(),v.y(),maxSpeed));
                         //保持球与摄像机相对位置不变
                         camera.position.x=mesh.position.x;
-                        camera.position.z=mesh.position.z-20;
+                        camera.position.z=mesh.position.z-10;
                         camera.up.x = 0;
                         camera.up.y = 1;
                         camera.up.z = 0;
@@ -251,7 +292,7 @@ Ammo().then(function(Ammo) {
                         body.setLinearVelocity(new Ammo.btVector3(v.x(),v.y(),v.z()+acceleratedSpeed));
                         //保持球与摄像机相对位置不变
                         camera.position.x=mesh.position.x;
-                        camera.position.z=mesh.position.z-20;
+                        camera.position.z=mesh.position.z-10;
                         camera.up.x = 0;
                         camera.up.y = 1;
                         camera.up.z = 0;
@@ -265,7 +306,7 @@ Ammo().then(function(Ammo) {
                         body.setLinearVelocity(new Ammo.btVector3(v.x(),v.y(),(0-maxSpeed)));
                         //保持球与摄像机相对位置不变
                         camera.position.x=mesh.position.x;
-                        camera.position.z=mesh.position.z-20;
+                        camera.position.z=mesh.position.z-10;
                         camera.up.x = 0;
                         camera.up.y = 1;
                         camera.up.z = 0;
@@ -276,7 +317,7 @@ Ammo().then(function(Ammo) {
                         body.setLinearVelocity(new Ammo.btVector3(v.x(),v.y(),v.z()-acceleratedSpeed));
                         //保持球与摄像机相对位置不变
                         camera.position.x=mesh.position.x;
-                        camera.position.z=mesh.position.z-20;
+                        camera.position.z=mesh.position.z-10;
                         camera.up.x = 0;
                         camera.up.y = 1;
                         camera.up.z = 0;
@@ -290,7 +331,7 @@ Ammo().then(function(Ammo) {
                         body.setLinearVelocity(new Ammo.btVector3(maxSpeed,v.y(),v.z()));
                         //保持球与摄像机相对位置不变
                         camera.position.x=mesh.position.x;
-                        camera.position.z=mesh.position.z-20;
+                        camera.position.z=mesh.position.z-10;
                         camera.up.x = 0;
                         camera.up.y = 1;
                         camera.up.z = 0;
@@ -301,7 +342,7 @@ Ammo().then(function(Ammo) {
                         body.setLinearVelocity(new Ammo.btVector3(v.x()+acceleratedSpeed,v.y(),v.z()));
                         //保持球与摄像机相对位置不变
                         camera.position.x=mesh.position.x;
-                        camera.position.z=mesh.position.z-20;
+                        camera.position.z=mesh.position.z-10;
                         camera.up.x = 0;
                         camera.up.y = 1;
                         camera.up.z = 0;
@@ -316,7 +357,7 @@ Ammo().then(function(Ammo) {
                             body.setLinearVelocity(new Ammo.btVector3((0-maxSpeed),v.y(),v.z()));
                             //保持球与摄像机相对位置不变
                             camera.position.x=mesh.position.x;
-                            camera.position.z=mesh.position.z-20;
+                            camera.position.z=mesh.position.z-10;
                             camera.up.x = 0;
                             camera.up.y = 1;
                             camera.up.z = 0;
@@ -327,7 +368,7 @@ Ammo().then(function(Ammo) {
                             body.setLinearVelocity(new Ammo.btVector3(v.x()-acceleratedSpeed,v.y(),v.z()));
                             //保持球与摄像机相对位置不变
                             camera.position.x=mesh.position.x;
-                            camera.position.z=mesh.position.z-20;
+                            camera.position.z=mesh.position.z-10;
                             camera.up.x = 0;
                             camera.up.y = 1;
                             camera.up.z = 0;
@@ -339,7 +380,7 @@ Ammo().then(function(Ammo) {
                         /*console.log(camera.position);*/
                         /*console.log(camera.lookAt);*/
                         camera.position.x=mesh.position.x;
-                        camera.position.z=mesh.position.z-20;
+                        camera.position.z=mesh.position.z-10;
                         camera.up.x = 0;
                         camera.up.y = 1;
                         camera.up.z = 0;
@@ -358,15 +399,20 @@ Ammo().then(function(Ammo) {
      * @param pos 位置
      * @param quat 四元数
      * @param w 宽
-     * @param l 长
-     * @param h 高
      * @param mass 质量
      * @param friction 摩擦力
+     * @param height 高度
+     * @param depth 长
+     * @return {Raycaster.params.Mesh | Mesh}
+     * @param material 材料
      */
-    function createBox(pos, quat, w, l, h, mass, friction) {
-        var material = mass > 0 ? materialDynamic : materialStatic;//材质
-        var shape = new THREE.BoxGeometry(w, l, h, 1, 1, 1);
-        var geometry = new Ammo.btBoxShape(new Ammo.btVector3(w * 0.5, l * 0.5, h * 0.5));
+    function createBox(pos, quat, w, height, depth, mass, friction,material) {
+        //console.log(!material);
+        if(!material){
+            material = mass > 0 ? materialDynamic : materialStatic;//材质
+        }
+        var shape = new THREE.BoxGeometry(w, height, depth, 1, 1, 1);
+        var geometry = new Ammo.btBoxShape(new Ammo.btVector3(w * 0.5, height * 0.5, depth * 0.5));
 
         if(!mass) mass = 0;
         if(!friction) friction = 1;
@@ -410,19 +456,130 @@ Ammo().then(function(Ammo) {
 
             syncList.push(sync);
         }
+        return mesh;
+    }
+    function createCylinder(pos,quat,radiusTop,radiusBottom,height,mass,friction) {
+        var material = new THREE.MeshPhongMaterial({color: 0xc0c0c0});
+        var geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, 64);
+        var btGeometry = new Ammo.btCylinderShape(new Ammo.btVector3(radiusTop, height, radiusBottom));
+        var cylinder = new THREE.Mesh(geometry, material);
+        cylinder.position.copy(pos);
+        cylinder.quaternion.copy(quat);
+        scene.add(cylinder);
+
+        //创建刚体，加入 physicsWorld
+        if (!mass) mass = 0;
+        if (!friction) friction = 1;
+        var transform = new Ammo.btTransform();
+        transform.setIdentity();
+        transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+        transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+        var motionState = new Ammo.btDefaultMotionState(transform);
+
+        var localInertia = new Ammo.btVector3(0, 0, 0);
+        btGeometry.calculateLocalInertia(mass, localInertia);
+
+        var rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, btGeometry, localInertia);
+        var body = new Ammo.btRigidBody(rbInfo);
+
+        //body.setFriction(friction);
+        //body.setRestitution(.9);
+        //body.setDamping(0.2, 0.2);
+
+        physicsWorld.addRigidBody(body);
     }
 
+    /**
+     * 创建障碍物----两个木箱
+     * @param pos 木箱需要放的位置
+     */
+    function createBarrier(pos) {
+        var textureCube=new THREE.TextureLoader().load('./textures/E_Holzbeschlag.bmp');
+        var newMaterial=new THREE.MeshPhongMaterial({map:textureCube});
+
+        var quaternion = new THREE.Quaternion(0, 0, 0, 1);
+        quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI / 5);
+        createBox(new THREE.Vector3(pos.x+0.1,pos.y+1.7,pos.z),quaternion,1.7,1.7,1.7,1,1,newMaterial);
+        quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI / 4);
+        createBox(new THREE.Vector3(pos.x,pos.y+3.4,pos.z),quaternion,1.7,1.7,1.7,1,1,newMaterial);
+    }
+    function createMap(){
+        //连续的 floor
+        var floorNumber=10;
+        var textureCube=new THREE.TextureLoader().load('./textures/Brick.bmp');
+        var newMaterial=new THREE.MeshPhongMaterial({map:textureCube});
+        for(let i=0;i<floorNumber;i++){
+            createBox(new THREE.Vector3(0,0,2*i),ZERO_QUATERNION,2,2,2,0,2,newMaterial);
+            lastObjectInMap.set('type','Box');
+            lastObjectInMap.set('position',[0,0,2*i]);
+        }
+        console.log(lastObjectInMap);
+        //两根圆柱
+        var cylinderNumber=10;
+        var quaternion = new THREE.Quaternion(0, 0, 0, 1);
+        quaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI / 2);
+        for(let i=0;i<cylinderNumber;i++){
+            createCylinder(new THREE.Vector3(-0.85,0.85,lastObjectInMap.get('position')[2]+2),quaternion,0.15,0.15,2,0,1);
+            createCylinder(new THREE.Vector3(0.85,0.85,lastObjectInMap.get('position')[2]+2),quaternion,0.15,0.15,2,0,1);
+            lastObjectInMap.set('type','CylinderDouble');
+            lastObjectInMap.set('position',[0,0,lastObjectInMap.get('position')[2]+2]);
+        }
+        //一根圆柱
+        cylinderNumber=5;
+        var y=-1.38+2.12*Math.sin(Math.acos(0.85/2.15));
+        for(let i=0;i<cylinderNumber;i++){
+            createCylinder(new THREE.Vector3(0,y,lastObjectInMap.get('position')[2]+2),quaternion,0.15,0.15,2,0,1);
+            lastObjectInMap.set('type','CylinderSingle');
+            lastObjectInMap.set('position',[0,0,lastObjectInMap.get('position')[2]+2]);
+        }
+        //两根圆柱
+        cylinderNumber=10;
+        for(let i=0;i<cylinderNumber;i++){
+            createCylinder(new THREE.Vector3(-0.85,0.85,lastObjectInMap.get('position')[2]+2),quaternion,0.15,0.15,2,0,1);
+            createCylinder(new THREE.Vector3(0.85,0.85,lastObjectInMap.get('position')[2]+2),quaternion,0.15,0.15,2,0,1);
+            lastObjectInMap.set('type','CylinderDouble');
+            lastObjectInMap.set('position',[0,0,lastObjectInMap.get('position')[2]+2]);
+        }
+        //连续的floor
+        for(let i=0;i<floorNumber;i++){
+            createBox(new THREE.Vector3(0,0,lastObjectInMap.get('position')[2]+2),ZERO_QUATERNION,2,2,2,0,2,newMaterial);
+            lastObjectInMap.set('type','Box');
+            lastObjectInMap.set('position',[0,0,lastObjectInMap.get('position')[2]+2]);
+        }
+        //几字形的floor
+        for(let i=0;i<floorNumber;i++){
+            createBox(new THREE.Vector3(lastObjectInMap.get('position')[0]+2,0,lastObjectInMap.get('position')[2]),ZERO_QUATERNION,2,2,2,0,2,newMaterial);
+            lastObjectInMap.set('type','Box');
+            lastObjectInMap.set('position',[lastObjectInMap.get('position')[0]+2,0,lastObjectInMap.get('position')[2]]);
+            //console.log(lastObjectInMap.get('position'));
+            if(i===floorNumber-1){
+                createBarrier(new THREE.Vector3(lastObjectInMap.get('position')[0],0,lastObjectInMap.get('position')[2]));
+                console.log(lastObjectInMap.get('position'));
+            }
+        }
+        for(let i=0;i<floorNumber;i++){
+            createBox(new THREE.Vector3(lastObjectInMap.get('position')[0],0,lastObjectInMap.get('position')[2]+2),ZERO_QUATERNION,2,2,2,0,2,newMaterial);
+            lastObjectInMap.set('type','Box');
+            lastObjectInMap.set('position',[lastObjectInMap.get('position')[0],0,lastObjectInMap.get('position')[2]+2]);
+        }
+        for(let i=0;i<floorNumber;i++){
+            createBox(new THREE.Vector3(lastObjectInMap.get('position')[0]-2,0,lastObjectInMap.get('position')[2]),ZERO_QUATERNION,2,2,2,0,2,newMaterial);
+            lastObjectInMap.set('type','Box');
+            lastObjectInMap.set('position',[lastObjectInMap.get('position')[0]-2,0,lastObjectInMap.get('position')[2]]);
+        }
+    }
     function createObjects() {
 
         //创建地图
-        createBox(new THREE.Vector3(0, -0.5, 0), ZERO_QUATERNION, 75, 1, 75, 0, 2);
-
+        createMap();
+        /*createBox(new THREE.Vector3(0, -0.5, 0), ZERO_QUATERNION, 5, 1, 5, 0, 2);
+        createBox(new THREE.Vector3(54.134429931640625, 10.012664794921875, 250), ZERO_QUATERNION, 5, 1, 5, 0, 2);*/
         /*var quaternion = new THREE.Quaternion(0, 0, 0, 1);
         quaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI / 18);
         createBox(new THREE.Vector3(0, -1.5, 0), quaternion, 8, 4, 10, 0);*/
 
         //创建球体
-        createSphere(new THREE.Vector3(0,2,0),ZERO_QUATERNION,1,32,32,5,1000);
+        createSphere(new THREE.Vector3(0,3,0),ZERO_QUATERNION,1,32,32,5,1000);
     }
 
     // - Init -
